84d30afa7d2160ac9fa514aa0982ab5ffc453b0a
datma <datma@D4T>
2023-11-27 14:56:08 +0700

    Add project files.

---
+++ KafkaTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+using Produce.Models;
+
+class Program
+{
+    static void Main()
+    {
+        // Kafka Configuration
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+        // Producer
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            Console.WriteLine("Producer : ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                var id = i+1;
+                var productModel = new Product { Id = id, Name = "Product " + id, status = true };
+                var sendMessage = JsonConvert.SerializeObject(productModel);
+
+                producer.Produce("test-topic", new Message<Null, string> { Value = sendMessage });
+                producer.Flush(TimeSpan.FromSeconds(10));
+                Console.WriteLine($"[Producer] Sent: {sendMessage}");
+            }
+        }
+    }
+}
---
+++ Produce/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Confluent.Kafka;
+using Consume.Models;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main()
+    {
+
+        // Consumer
+        var configC = new ConsumerConfig
+        {
+            GroupId = "test_groupId",
+            BootstrapServers = "localhost:9092",
+            //AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+        using (var consumer = new ConsumerBuilder<Null, string>(configC).Build())
+        {
+            consumer.Subscribe("test-topic");
+
+            Console.WriteLine("Consumer: ");
+
+            while (true)
+            {
+                try
+                {
+                    var consumeResult = consumer.Consume();
+                    var receivedMessage = consumeResult.Message.Value;
+
+                    var productModel = JsonConvert.DeserializeObject<Product>(receivedMessage);
+                    Console.WriteLine($"[Consumer] Received: Id={productModel.Id}, Name={productModel.Name},Status={productModel.status}");
+                }
+                catch (ConsumeException e)
+                {
+                    Console.WriteLine($"Error while consuming: {e.Error.Reason}");
+                }
+            }
+
+        }
+    }
+}
+
